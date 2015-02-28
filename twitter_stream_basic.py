5843f87f463de8c8c9eed76680a54028e532bdde
maheshgudi <mahesh.p.gudi@gmail.com>
2015-02-28 16:47:58 +0530

    Changed codes for searching keywords

--- twitter_stream_basic.py
+++ twitter_stream_basic.py
@@ -1,39 +1,30 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy import StreamListener
-import time
-from nltk import *
+from TwitterSearch import *
 
 ckey = "your customer key"
 csecret = "your customer key secret"
 atoken = "your access token"
 atoksec = "your access token secret"
 
-keyword = raw_input("Enter the keyword to search: ")
+def search():
+            
+    try:
+        i =1
+        keyword = raw_input("Enter the keyword to search: ")
 
-class listener(StreamListener):
+        tso.set_keywords([keyword])
+        tso.set_language('en')
+        tso.set_include_entities(False)
 
-    def on_data(self,data):
-        try:
-            #print data
+        ts = TwitterSearch(ckey,csecret,atoken,atoksec)
 
-            tweet = data.split(',"text":"')[1].split('","source')[0]
-            print tweet
-            stat_token = word_tokenize(tweet)
-            stat_pos = pos_tag(stat_token)            
-            print stat_pos
-            return True
         
-
-        except BaseException, e:
-            print 'failed,', str(e)
-            time.sleep(5)
-
-    def on_error(self, status):
-        print status
-
-auth = OAuthHandler(ckey,csecret)
-auth.set_access_token(atoken,atoksec)
-twitterStream = Stream(auth, listener())
-
-twitterStream.filter(track=[keyword])
+        for tweet in ts.search_tweets_iterable(tso):
+            print( i, '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+            i+=1
+            if i == 31:
+                break 
+        
+    except TwitterSearchException as e:
+        print (e)
+            
+search()
